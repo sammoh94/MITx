80641379137165c77bb43272f76d1289bd3d9179
OpenShift guest <samarthdps@gmail.com>
2013-06-10 14:42:34 -0400

    Finished read_operand. Working on evaluate

--- day1.js
+++ day1.js
@@ -2,9 +2,66 @@
 /*
 a calculator that calculates numbers
 */
-function calculate(){
-    var input = $('#text1:first');// get input field from DOM
-    var val = input.val();
-    var output = $('#text1_out:first');
-    output.text(val);
+function calculate(text){
+    var pattern = /\d+|\+|\-|\*|\/|\(|\)/g; //d+ is matching digits that atleast have a length of 1
+    var tokens = text.match(pattern)
+    return JSON.stringify(tokens);
 }
+/*
+a function that interprets the first token in the array
+*/
+function read_operand(token_array){
+    var num  = token_array.shift();
+    if (isNaN(parseInt(num))){
+        throw "number expected";
+    }
+    return(parseInt(num));
+}
+
+function evaluation(token_array){
+    if (token_array.length === 0){
+        throw "missing operand";
+    }
+    var value_1 = read_operand(token_array);
+    while (token_array.length !== 0) {
+        var operator = token_array.shift();
+        if (operator === '+'){
+            continue;
+        }
+        if (operator === '-'){
+            continue;
+        }
+        if (operator === '*'){
+            continue;
+        }
+        if (operator === '/'){
+            continue;
+        }
+        else{
+            throw "unrecognized operator";
+        }
+        if (token_array === 0){
+            throw "missing operand";
+        }
+        var temp = read_operand(token_array);
+        var value_2 = value_1+operator+temp; 
+    }
+    return value_2
+}
+
+function setup_calc(div){
+    var input = $('<input></input>',{type: "text", size: 50});
+    var output = $('<div></div>');
+    var button  = $('<button>Calculate</button>');
+    button.bind("click", function (){
+       output.text(String(calculate(input.val()))); 
+    });
+    $(div).append(input, button, output);
+}
+
+$(document).ready(function (){
+   $('.calculator').each(function(){  // '.' is class, # is id
+       // this refers to the <div> with class calculator
+       setup_calc(this);
+   });  
+});
