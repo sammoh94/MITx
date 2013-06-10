9f9828c72ac40ef8ec7cce54bd34ccb7a33c6860
OpenShift guest <samarthdps@gmail.com>
2013-06-10 19:56:03 -0400

    #1 and #2 are working. #4 is untested

--- day1.js
+++ day1.js
@@ -21,20 +21,66 @@ function calculate(text){
 a function that interprets the first token in the array
 */
 function read_operand(token_array){
-    var num  = token_array.shift();
-    if (isNaN(parseInt(num))){
+    var symbol  = token_array[0];
+    var num = parseInt(symbol,10);
+    if (symbol === '('){
+        token_array.shift();
+        num = evaluation(token_array);
+    }
+    else if (symbol == ")"){
+        return ")";
+    }
+    else if (isNaN(num)){
         throw "number expected";
     }
-    return(parseInt(num));
-}
+    else if (symbol === '-'){
+        token_array.shift();
+        num = parseFloat(token_array[0],10);
+        token_array.shift();
+        return -1*num;
+    }
+    else{
+        token_array.shift();
+    }
+    return num;
+    }
+    
 
+/*
+function read_term(token_array){
+    for (var i = 0; i < token_array.length; i++) {
+        if (token_array[i]==='+' || token_array[i]==='-'){   
+            break;
+    }
+    else if (token_array.length === 0){
+        break;
+    }
+    }
+    var value_1 = read_operand(token_array);
+    while (token_array.length !== 0) {
+        var operator = token_array.shift()
+        var temp = read_operand(token_array);
+        
+        if (operator === '*'){
+            var value_2 = parseInt(value_1) * parseInt(temp);
+        }
+        else if (operator === '/'){
+            value_2 = parseInt(value_1) / parseInt(temp);
+        }
+}
+}
+*/
 function evaluation(token_array){
     if (token_array.length === 0){
         throw "missing operand";
     }
     var value_1 = read_operand(token_array);
     while (token_array.length !== 0) {
-        var operator = token_array.shift()
+        if (token_array[0] === ')'){
+            token_array.shift();
+            return value_1;
+        }
+        var operator = token_array.shift();
         var temp = read_operand(token_array);
         
         if (operator === '+'){
@@ -54,6 +100,7 @@ function evaluation(token_array){
         }
     }    
     return value_2;
+
 }
 
 function setup_calc(div){
