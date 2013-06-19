92ec31cedf55c70a75ef74e3d52e314037629fdf
Samarth Mohan <smohan94@mit.edu>
2013-06-19 10:38:55 -0400

    using y_scale function

--- visualizations/visual.js
+++ visualizations/visual.js
@@ -7,22 +7,24 @@ var data = [0,4,6,8,10,12,15];
 // data.forEach(function(d) {
 // 	chart.append($('<div></div>').css("width",d*10+"px").text(d));
 // });
+var chart_height = 140;
 
 var x_scale = d3.scale.linear().domain([0,d3.max(data)]).range(["0%","100%"]);
+var y_scale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0,chart_height]);
 
 
 var chart = d3.select(".chart-container").append("svg").attr("class","chart");
 
 chart.selectAll("rect").data(data)
 	.enter().append("rect")
-	.attr("y",function(d,i){return 20*i;})
+	.attr("y",function(d,i){return y_scale(i);})
 	.attr("width",x_scale)
 	.attr("height",20);
 
 chart.selectAll("text").data(data)
 	.enter().append("text")
 	.attr("x",x_scale)
-	.attr("y",function(d,i){return (i+.5)*20;})
+	.attr("y",function(d,i){return y_scale(i) + y_scale.rangeBand()/2;})
 	.attr("dx",-3)
 	.attr("dy",".35em")
 	.attr("text-anchor","end")
